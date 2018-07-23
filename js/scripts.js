5d80ddeb529235d75a84fc859133d3b77af45ece
melisamalala <melissamalala@gmail.com>
2018-07-23 10:31:04 +0300

    Add background image to header

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-// function w3_open() {
-//     document.getElementById("mySidebar").style.width = "100%";
-//     document.getElementById("mySidebar").style.display = "block";
-// }
-// function w3_close() {
-//     document.getElementById("mySidebar").style.display = "none";
-// }
+function w3_open() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
 
 // HAPPY CUSTOMERS SCRIPT
 
@@ -37,4 +38,27 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+// DROPDOWN MENU
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
